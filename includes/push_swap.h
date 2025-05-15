37ad9af34836cb85597613e76dd94c1562bd043b
Thashiru Chandupa Fabio Vitharana <tvithara@e3r7p3.42roma.it>
2025-05-15 17:25:47 +0200

    updated

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,8 +6,18 @@
 /*   By: tvithara <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 16:49:54 by tvithara          #+#    #+#             */
-/*   Updated: 2025/05/15 16:49:59 by tvithara         ###   ########.fr       */
+/*   Updated: 2025/05/15 17:19:31 by tvithara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+typedef struct s_list
+{
+	int	value;
+	int	index;
+	struct s_list *next;
+}	t_list;
 
 
